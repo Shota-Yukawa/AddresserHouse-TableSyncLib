264be5e4a0c079e46058c2900f731a2ad08a1852
Shota-Yukawa <s.progdes@gmail.com>
2024-03-03 22:16:08 +0900

    定数名の変更

--- src/main/java/com/ah/tablesynclib/rest/SyncRequestService.java
+++ src/main/java/com/ah/tablesynclib/rest/SyncRequestService.java
@@ -18,8 +18,10 @@ public class SyncRequestService {
 
 	private final RestTemplate rest = new RestTemplate();
 	
-	private final String TALBENAME_APARTROWNER = "apartowners";
-	private final String TALBENAME_APARTMENT = "apartments";
+	private final String TALBENAME_APARTROWNERS = "apartowners";
+	private final String TALBENAME_APARTMENTS = "apartments";
+	
+	private final String TABLENAME_ADDRESSERRESIDENCES = "addresser_residences";
 
 	/**
 	 * [apartowner登録時] table syncサービスへapartownerテーブルの同期リクエストを送る。
@@ -27,7 +29,7 @@ public class SyncRequestService {
 	 * @return
 	 */
 	public String syncApartownerInsert(TableSyncRequestBean body) {
-		String url = endpointProperties.getTableSyncInsertUrl(TALBENAME_APARTROWNER);
+		String url = endpointProperties.getTableSyncInsertUrl(TALBENAME_APARTROWNERS);
 		RequestEntity<TableSyncRequestBean> reqEntity = entityFactory.getPostEntity(url, body);
 
 		ResponseEntity<String> res = rest.exchange(reqEntity, String.class);
@@ -40,7 +42,7 @@ public class SyncRequestService {
 	 * @return
 	 */
 	public String syncApartownerUpdate(TableSyncRequestBean body) {
-		String url = endpointProperties.getTableSyncUpdateUrl(TALBENAME_APARTROWNER);
+		String url = endpointProperties.getTableSyncUpdateUrl(TALBENAME_APARTROWNERS);
 		RequestEntity<TableSyncRequestBean> reqEntity = entityFactory.getPutEntity(url, body);
 
 		ResponseEntity<String> res = rest.exchange(reqEntity, String.class);
@@ -53,7 +55,7 @@ public class SyncRequestService {
 	 * @return
 	 */
 	public String syncApartownerDelete(Integer apartownerId) {
-		String url = endpointProperties.getTableSyncDeleteUrl(TALBENAME_APARTROWNER, apartownerId);
+		String url = endpointProperties.getTableSyncDeleteUrl(TALBENAME_APARTROWNERS, apartownerId);
 		RequestEntity<Void> reqEntity = entityFactory.getDeleteEntity(url);
 
 		ResponseEntity<String> res = rest.exchange(reqEntity, String.class);
@@ -66,7 +68,7 @@ public class SyncRequestService {
 	 * @return
 	 */
 	public String syncApartmentInsert(TableSyncRequestBean body) {
-		String url = endpointProperties.getTableSyncInsertUrl(TALBENAME_APARTMENT);
+		String url = endpointProperties.getTableSyncInsertUrl(TALBENAME_APARTMENTS);
 		RequestEntity<TableSyncRequestBean> reqEntity = entityFactory.getPostEntity(url, body);
 
 		ResponseEntity<String> res = rest.exchange(reqEntity, String.class);
@@ -79,7 +81,7 @@ public class SyncRequestService {
 	 * @return
 	 */
 	public String syncApartmentUpdate(TableSyncRequestBean body) {
-		String url = endpointProperties.getTableSyncUpdateUrl(TALBENAME_APARTMENT);
+		String url = endpointProperties.getTableSyncUpdateUrl(TALBENAME_APARTMENTS);
 		RequestEntity<TableSyncRequestBean> reqEntity = entityFactory.getPutEntity(url, body);
 
 		ResponseEntity<String> res = rest.exchange(reqEntity, String.class);
@@ -92,7 +94,7 @@ public class SyncRequestService {
 	 * @return
 	 */
 	public String syncApartmentDelete(Integer apartownerId) {
-		String url = endpointProperties.getTableSyncDeleteUrl(TALBENAME_APARTMENT, apartownerId);
+		String url = endpointProperties.getTableSyncDeleteUrl(TALBENAME_APARTMENTS, apartownerId);
 		RequestEntity<Void> reqEntity = entityFactory.getDeleteEntity(url);
 
 		ResponseEntity<String> res = rest.exchange(reqEntity, String.class);
